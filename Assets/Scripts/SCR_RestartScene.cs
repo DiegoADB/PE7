2f252e4fd819a2bcb4699d7cd30c9a1981cbcbbe
DiegoADB <diegoadb17@gmail.com>
2018-04-04 15:43:51 -0500

    First Commit
    Player Controller

---
+++ Assets/Scripts/SCR_RestartScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SCR_RestartScene : MonoBehaviour {
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+            SceneManager.LoadSceneAsync(0);
+    }
+}
---
+++ Assets/Scripts/SCR_SpeedBoost.cs
@@ -0,0 +1,15 @@
+﻿//Made by Diego Diaz
+//diegodiaz@golstats.com
+using UnityEngine;
+
+public class SCR_SpeedBoost : MonoBehaviour 
+{
+    public float boostDuration;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<SCR_CharacterMotor>())
+            other.GetComponent<SCR_CharacterMotor>().SpeedBoost(boostDuration);
+    }
+
+}
