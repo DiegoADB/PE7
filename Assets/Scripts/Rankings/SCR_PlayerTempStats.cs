607f1b84b7793594f54cf27cd60fde46231a1b29
AliceBearmaid <31712870+AliceBearmaid@users.noreply.github.com>
2018-04-04 16:48:05 -0500

    Pista 8 + Rankings

---
+++ Assets/Scripts/Rankings/SCR_PlayerTempStats.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_PlayerTempStats : MonoBehaviour {
+
+    public int myPlace = 0;
+    public int myScore = 0;
+    int tempScore;
+    public float distanceToNext;
+    public GameObject[] myCheckpoints;
+    //[HideInInspector]
+    public GameObject nextTarget;
+    [HideInInspector]
+    public GameObject pastTarget;
+
+    private void Start()
+    {
+        nextTarget = myCheckpoints[0];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject==nextTarget)
+        {
+            myScore++;
+            tempScore++;
+            pastTarget = nextTarget;
+            tempScore = myScore;
+
+            if (tempScore>7)
+            {
+                tempScore = 0;
+            }
+            nextTarget = myCheckpoints[tempScore];
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Rankings/SCR_Ranking.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Ranking : MonoBehaviour {
+
+    public GameObject[] players;
+    SCR_PlayerTempStats[] myStats;
+    List<SCR_PlayerTempStats> mySortingList;
+    public int playerNum;
+
+
+    void Start()
+    {
+        myStats = new SCR_PlayerTempStats[playerNum];
+        mySortingList = new List<SCR_PlayerTempStats>();
+
+        for (int i = 0; i < playerNum; i++)
+        {
+            myStats[i] = players[i].GetComponent<SCR_PlayerTempStats>();
+            mySortingList.Add(myStats[i]);
+
+        }
+        StartCoroutine(CheckPositions());
+
+    }
+
+    IEnumerator CheckPositions()
+    {
+        for(int i =0; i<playerNum;i++)
+        {
+            myStats[i].distanceToNext = Vector3.Distance(players[i].transform.position, myStats[i].nextTarget.transform.position);
+        }
+        mySortingList.Sort(SortByScore);
+        mySortingList.Sort(SortByDist);
+
+        for (int i = 0; i < mySortingList.Count; i++)
+        {
+            
+            mySortingList[i].myPlace = i+1;
+        }
+
+        yield return new WaitForSeconds(0.25f);
+
+        StartCoroutine(CheckPositions());
+    }
+
+    public int SortByDist(SCR_PlayerTempStats a, SCR_PlayerTempStats b)
+    {
+        if(a.myScore==b.myScore)
+        {
+            return a.distanceToNext.CompareTo(b.distanceToNext);
+        }
+        else
+        {
+            return a.distanceToNext.CompareTo(a.distanceToNext);
+        }
+    }
+
+    public int SortByScore(SCR_PlayerTempStats a, SCR_PlayerTempStats b)
+    {
+
+        return a.myScore.CompareTo(b.myScore);
+    }
+
+
+}
