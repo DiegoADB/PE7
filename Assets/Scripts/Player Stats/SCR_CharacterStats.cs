97961b71b356c374f10fe10cf5fb2f257a98552a
DiegoADB <diegoadb17@gmail.com>
2018-04-07 18:30:01 -0500

    Se agrego Script de Stats

---
+++ Assets/Scripts/Player Stats/SCR_CharacterStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_CharacterStats : MonoBehaviour 
+{
+    private float playerHP;
+
+    public float GetCurrentHP()
+    {
+        return playerHP;
+    }
+
+    public void DamagePlayer(float _dmg)
+    {
+        playerHP -= _dmg;
+        if (playerHP <= 0)
+        {
+            //TODO: Explode
+        }
+    }
+}
