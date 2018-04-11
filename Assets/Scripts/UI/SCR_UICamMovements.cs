2c483cf020581e7b47c873b1416e11d0e77aad19
Bernardo <bernardogaribaybravo@gmail.com>
2018-04-11 17:05:55 -0500

    Updated UI Animations

---
+++ Assets/Scripts/UI/SCR_UICamMovements.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_UICamMovements : MonoBehaviour {
+
+    public Transform mainTransform;
+    public Transform optionsTransform;
+
+
+    public void OnOptionsClicked()
+    {
+        Debug.Log("I HAVE BEEN CLICKED");
+        StartCoroutine(MoveCamera(optionsTransform));
+    }
+
+    public void OnBackToMainClicked()
+    {
+        StartCoroutine(MoveCamera(mainTransform));
+    }
+
+    IEnumerator MoveCamera(Transform _endPos)
+    {
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, _endPos.position, 5.0f * Time.deltaTime);
+        Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, _endPos.rotation, 5.0f * Time.deltaTime);
+        yield return new WaitForSeconds(0.05f);
+        StartCoroutine(MoveCamera(_endPos));
+    }
+}
