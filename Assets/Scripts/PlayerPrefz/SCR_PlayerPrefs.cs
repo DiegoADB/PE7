d0133cf2ca306002120d324f6305ac853c07f17b
DiegoADB <diegoadb17@gmail.com>
2018-06-25 16:54:40 -0500

    Player Prefs

---
+++ Assets/Scripts/PlayerPrefz/SCR_PlayerPrefs.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_PlayerPrefs : MonoBehaviour
+{
+    public static SCR_PlayerPrefs instance = null;
+    public GameObject[] penguins;
+    public float currency;
+    public int myPenguin;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
