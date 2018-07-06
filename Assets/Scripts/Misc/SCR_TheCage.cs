661e4bdb31dc2c2dacbf45b0bd74479298ef32d0
Bernardo Garibay <bernardogaribaybravo@gmail.com>
2018-07-06 12:45:34 -0500

    Inicio de la carrera

---
+++ Assets/Scripts/Misc/SCR_TheCage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SCR_TheCage : MonoBehaviour {
+
+    public float waitTime;
+
+    public GameObject cage;
+    public Text countdownText;
+
+
+    private void FixedUpdate()
+    {
+        waitTime -= Time.fixedDeltaTime;
+        countdownText.text = Mathf.FloorToInt(waitTime).ToString();
+        if (Mathf.FloorToInt(waitTime) == 0)
+        {
+            countdownText.text = "GO!";
+        }
+        if (waitTime <= 0)
+        {
+            countdownText.text = "";
+            Destroy(cage);
+            Destroy(gameObject);
+        }
+    }
+}
