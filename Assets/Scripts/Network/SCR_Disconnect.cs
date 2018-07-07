e39925fbefa2857a921a4da64a2e8012dda3df79
DiegoADB <diegoadb17@gmail.com>
2018-07-07 00:06:05 -0500

    Disconnect From Match script

---
+++ Assets/Scripts/Network/SCR_Disconnect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class SCR_Disconnect : MonoBehaviour
+{
+    public static void DisconnectFromMatch()
+    {
+        NetworkManager manager = NetworkManager.singleton;
+        MatchInfo match = manager.matchInfo;
+        manager.matchMaker.DropConnection(match.networkId, match.nodeId, 0, manager.OnDropConnection);
+        manager.StopHost();
+        SceneManager.LoadScene("Menu");
+    }
+}
