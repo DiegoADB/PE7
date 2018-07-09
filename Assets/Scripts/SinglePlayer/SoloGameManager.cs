9604e83bc891a9ebc030f2962a09199638937541
MasterMeme <b_gonzalez_09@hotmail.com>
2018-07-09 17:00:15 -0500

    asdfbañsdbfñlasñdlksanlk
    xddx

---
+++ Assets/Scripts/SinglePlayer/SoloGameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoloGameManager : MonoBehaviour {
+
+    public GameObject[] StartPos;
+    public SCR_TheCage cage;
+    public GameObject myPlayer;
+    public GameObject theEnemy;
+
+    GameObject thePlayer;
+    private void Start()
+    {
+        Invoke("Despabila", cage.waitTime);
+        for(int i = 0; i< StartPos.Length;i++)
+        {
+            if (i == 0)
+            {
+                thePlayer = Instantiate(myPlayer, StartPos[i].transform.position, Quaternion.identity);
+                thePlayer.GetComponent<SCR_CharacterMotor_Net>().enabled = false;
+                thePlayer.SetActive(true);
+            }
+            else
+            {
+                Instantiate(theEnemy, StartPos[i].transform.position, Quaternion.identity).SetActive(true);
+            }
+        }
+    }
+
+	
+    void Despabila()
+    {
+        thePlayer.GetComponent<SCR_CharacterMotor_Net>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
