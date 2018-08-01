907f51d4ddb32b102304148bc5c1770e501bcb67
DiegoADB <diegoadb17@gmail.com>
2018-08-01 03:29:28 -0500

    Checkpoints

---
+++ Assets/Scripts/Items/Net/SCR_ItemRotation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_ItemRotation : MonoBehaviour {
+
+	void Update ()
+    {
+        transform.Rotate(transform.up * 2);
+	}
+}
